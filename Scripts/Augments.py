c7a0979b3a04f6728c2b9d73fa5d7655632bab17
Ofir Mazor <66764040+OfirMazor@users.noreply.github.com>
2023-02-06 15:32:57 +0200

    Update Augments.py

--- Scripts/Augments.py
+++ Scripts/Augments.py
@@ -1,55 +1,73 @@
 import albumentations as A
 from torchvision import transforms as T
 import cv2
-
 import Configs
 
 
 
+def train_augmentor():
+  train_augmentor = A.Compose([  
+                               A.Resize(height        = Configs.resize_height,
+                                        width         = Configs.resize_width,
+                                        interpolation = cv2.INTER_NEAREST),
 
-train_augmentor = A.Compose([  
-                             A.Resize(height        = Configs.resize_height,
-                                      width         =  Configs.resize_width,
-                                      interpolation = cv2.INTER_NEAREST),
-    
-                             A.HorizontalFlip(),
-    
-                             A.VerticalFlip(),
-    
-                             A.GridDistortion(p = 0.2),
-    
-                             A.RandomBrightnessContrast(brightness_limit = (0,0.5),
-                                                        contrast_limit   = (0,0.5)),
-                             A.GaussNoise()
-                            ])
+                               A.HorizontalFlip(),
 
+                               A.VerticalFlip(),
 
+                               A.GridDistortion(p = 0.2),
 
-valid_augmentor = A.Compose([
-                             A.Resize(height        = Configs.resize_height,
-                                      width         = Configs.resize_width,
-                                      interpolation = cv2.INTER_NEAREST),
-    
-                             A.HorizontalFlip(),
-    
-                             A.GridDistortion(p = 0.2)
-                            ])
+                               A.RandomBrightnessContrast(brightness_limit = (0,0.5),
+                                                          contrast_limit   = (0,0.5)),
+                               A.GaussNoise()
+                              ])
+ return train_augmentor
 
 
 
 
-test_augmentor = A.Compose([
-                            A.Resize(height        = Configs.resize_height,
-                                     width         = Configs.resize_width,
-                                     interpolation = cv2.INTER_NEAREST)
-                           ])
 
 
 
 
-normalizer_transform = T.Compose([
-                                 T.ToTensor(),
-                                 
-                                 T.Normalize(Configs.normalize_mean,
-                                             Configs.normalize_std)
-                                 ])
+def valid_augmentor():
+  valid_augmentor = A.Compose([
+                               A.Resize(height        = Configs.resize_height,
+                                        width         = Configs.resize_width,
+                                        interpolation = cv2.INTER_NEAREST),
+
+                               A.HorizontalFlip(),
+
+                               A.GridDistortion(p = 0.2)
+                              ])
+return valid_augmentor
+
+
+
+
+
+
+
+def test_augmentor():
+  test_augmentor = A.Compose([ 
+                              A.Resize(height        = Configs.resize_height,
+                                       width         = Configs.resize_width,
+                                       interpolation = cv2.INTER_NEAREST)
+                             ])
+return test_augmentor
+
+
+
+
+
+
+
+def normalizer_transform():
+  normalizer_transform = T.Compose([
+                                   T.ToTensor(),
+
+                                   T.Normalize(Configs.normalize_mean,
+                                               Configs.normalize_std)
+                                   ])
+return normalizer_transform
+
